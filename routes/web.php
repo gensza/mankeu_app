f4431a73f7d805b374658c218c1582e8ee90459e
gensza <mohamadgenszavernando@gmail.com>
2025-07-19 17:50:51 +0700

    feat : install laravel auth breeze

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+use App\Livewire\Counter;
+
+Route::get('/counter', Counter::class);
+
+require __DIR__ . '/auth.php';
