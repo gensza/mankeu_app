84fe2d649493b8ed698a01ff7de18fe351756795
gensza <mohamadgenszavernando@gmail.com>
2025-10-17 20:07:13 +0700

    feat : update data dashboard

--- app/Livewire/Dashboards/Index.php
+++ app/Livewire/Dashboards/Index.php
@@ -3,11 +3,38 @@
 namespace App\Livewire\Dashboards;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.dashboards.index');
+        $datas['count_accounts'] = DB::table('accounts')->count();
+
+        $datas['totalIncome'] = DB::table('journal_entry_lines')
+            ->join('accounts', 'journal_entry_lines.account_id', '=', 'accounts.id')
+            ->where('accounts.type', 'revenue')
+            ->sum('journal_entry_lines.credit');
+
+        $datas['totalExpense'] = DB::table('journal_entry_lines')
+            ->join('accounts', 'journal_entry_lines.account_id', '=', 'accounts.id')
+            ->where('accounts.type', 'expense')
+            ->sum('journal_entry_lines.debit');
+
+        $monthlyIncome = DB::table('journal_entry_lines')
+            ->join('journal_entries', 'journal_entry_lines.journal_entry_id', '=', 'journal_entries.id')
+            ->join('accounts', 'journal_entry_lines.account_id', '=', 'accounts.id')
+            ->where('accounts.type', 'revenue')
+            ->selectRaw('
+        DATE_FORMAT(journal_entries.date, "%Y-%m") as month,
+        SUM(journal_entry_lines.credit) as total_income
+    ')
+            ->groupBy('month')
+            ->orderBy('month', 'asc')
+            ->get();
+
+        $datas['labels_grafik'] = $monthlyIncome->pluck('month');
+        $datas['data_grafik'] = $monthlyIncome->pluck('total_income');
+        return view('livewire.dashboards.index', ['datas' => $datas]);
     }
 }
--- resources/views/livewire/dashboards/index.blade.php
+++ resources/views/livewire/dashboards/index.blade.php
@@ -15,33 +15,14 @@
                 <div class="card-body">
                     <div class="row align-items-center">
                         <div class="col-icon">
-                            <div class="icon-big text-center icon-primary bubble-shadow-small">
-                                <i class="fas fa-users"></i>
-                            </div>
-                        </div>
-                        <div class="col col-stats ms-3 ms-sm-0">
-                            <div class="numbers">
-                                <p class="card-category">Visitors</p>
-                                <h4 class="card-title">1,294</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-3">
-            <div class="card card-stats card-round">
-                <div class="card-body">
-                    <div class="row align-items-center">
-                        <div class="col-icon">
                             <div class="icon-big text-center icon-info bubble-shadow-small">
-                                <i class="fas fa-user-check"></i>
+                                <i class="fas fa-tag"></i>
                             </div>
                         </div>
                         <div class="col col-stats ms-3 ms-sm-0">
                             <div class="numbers">
-                                <p class="card-category">Subscribers</p>
-                                <h4 class="card-title">1303</h4>
+                                <p class="card-category">Accounts</p>
+                                <h4 class="card-title">{{ $datas['count_accounts'] }}</h4>
                             </div>
                         </div>
                     </div>
@@ -54,13 +35,13 @@
                     <div class="row align-items-center">
                         <div class="col-icon">
                             <div class="icon-big text-center icon-success bubble-shadow-small">
-                                <i class="fas fa-luggage-cart"></i>
+                                <i class="fas fa-wallet"></i>
                             </div>
                         </div>
                         <div class="col col-stats ms-3 ms-sm-0">
                             <div class="numbers">
-                                <p class="card-category">Sales</p>
-                                <h4 class="card-title">$ 1,345</h4>
+                                <p class="card-category">Income</p>
+                                <h4 class="card-title">Rp {{ number_format($datas['totalIncome'], 0, ',', '.') }}</h4>
                             </div>
                         </div>
                     </div>
@@ -73,13 +54,13 @@
                     <div class="row align-items-center">
                         <div class="col-icon">
                             <div class="icon-big text-center icon-secondary bubble-shadow-small">
-                                <i class="far fa-check-circle"></i>
+                                <i class="fas fa-money-check"></i>
                             </div>
                         </div>
                         <div class="col col-stats ms-3 ms-sm-0">
                             <div class="numbers">
-                                <p class="card-category">Order</p>
-                                <h4 class="card-title">576</h4>
+                                <p class="card-category">Expense</p>
+                                <h4 class="card-title">Rp {{ number_format($datas['totalExpense'], 0, ',', '.') }}</h4>
                             </div>
                         </div>
                     </div>
@@ -92,7 +73,7 @@
             <div class="card card-round">
                 <div class="card-header">
                     <div class="card-head-row">
-                        <div class="card-title">User Statistics</div>
+                        <div class="card-title">Income Statistics</div>
                         <div class="card-tools">
                             <a href="#" class="btn btn-label-success btn-round btn-sm me-2">
                                 <span class="btn-label">
@@ -173,5 +154,119 @@
             lineColor: "#177dff",
             fillColor: "rgba(23, 125, 255, 0.14)",
         });
+
+        var ctx = document.getElementById('statisticsChart').getContext('2d');
+
+        var statisticsChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: @json($datas['labels_grafik']),
+                datasets: [{
+                        label: "Income",
+                        borderColor: '#f3545d',
+                        pointBackgroundColor: 'rgba(243, 84, 93, 0.6)',
+                        pointRadius: 0,
+                        backgroundColor: 'rgba(243, 84, 93, 0.4)',
+                        legendColor: '#f3545d',
+                        fill: true,
+                        borderWidth: 2,
+                        data: @json($datas['data_grafik'])
+                    },
+                    // {
+                    //     label: "New Visitors",
+                    //     borderColor: '#fdaf4b',
+                    //     pointBackgroundColor: 'rgba(253, 175, 75, 0.6)',
+                    //     pointRadius: 0,
+                    //     backgroundColor: 'rgba(253, 175, 75, 0.4)',
+                    //     legendColor: '#fdaf4b',
+                    //     fill: true,
+                    //     borderWidth: 2,
+                    //     data: [256, 230, 245, 287, 240, 250, 230, 295, 331, 431, 456, 521]
+                    // }, {
+                    //     label: "Active Users",
+                    //     borderColor: '#177dff',
+                    //     pointBackgroundColor: 'rgba(23, 125, 255, 0.6)',
+                    //     pointRadius: 0,
+                    //     backgroundColor: 'rgba(23, 125, 255, 0.4)',
+                    //     legendColor: '#177dff',
+                    //     fill: true,
+                    //     borderWidth: 2,
+                    //     data: [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 900]
+                    // }
+                ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    bodySpacing: 4,
+                    mode: "nearest",
+                    intersect: 0,
+                    position: "nearest",
+                    xPadding: 10,
+                    yPadding: 10,
+                    caretPadding: 10
+                },
+                layout: {
+                    padding: {
+                        left: 5,
+                        right: 5,
+                        top: 15,
+                        bottom: 15
+                    }
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            fontStyle: "500",
+                            beginAtZero: false,
+                            maxTicksLimit: 5,
+                            padding: 10
+                        },
+                        gridLines: {
+                            drawTicks: false,
+                            display: false
+                        }
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            zeroLineColor: "transparent"
+                        },
+                        ticks: {
+                            padding: 10,
+                            fontStyle: "500"
+                        }
+                    }]
+                },
+                legendCallback: function(chart) {
+                    var text = [];
+                    text.push('<ul class="' + chart.id + '-legend html-legend">');
+                    for (var i = 0; i < chart.data.datasets.length; i++) {
+                        text.push('<li><span style="background-color:' + chart.data.datasets[i].legendColor +
+                            '"></span>');
+                        if (chart.data.datasets[i].label) {
+                            text.push(chart.data.datasets[i].label);
+                        }
+                        text.push('</li>');
+                    }
+                    text.push('</ul>');
+                    return text.join('');
+                }
+            }
+        });
+
+        var myLegendContainer = document.getElementById("myChartLegend");
+
+        // generate HTML legend
+        myLegendContainer.innerHTML = statisticsChart.generateLegend();
+
+        // bind onClick event to all LI-tags of the legend
+        var legendItems = myLegendContainer.getElementsByTagName('li');
+        for (var i = 0; i < legendItems.length; i += 1) {
+            legendItems[i].addEventListener("click", legendClickCallback, false);
+        }
     </script>
 @endpush
